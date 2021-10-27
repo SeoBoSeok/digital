a20d8af41a5778d9952208f2b7a0b36e54b15d66
SeoBoSeok <ggy7bbo@gmail.com>
2021-10-27 09:29:19 +0900

    hot fix

---
+++ theme/basic/skin/content/schedule/content.skin.php
@@ -0,0 +1,135 @@
+<?php
+if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+
+// add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
+add_stylesheet('<link rel="stylesheet" href="'.$content_skin_url.'/style.css">', 0);
+?>
+
+<!-- <article id="ctt" class="ctt_<?php //echo $co_id; ?>">
+    <header>
+        <h1><?php//echo $g5['title']; ?></h1>
+    </header>
+
+    <div id="ctt_con">
+        <?php //echo $str; ?>
+    </div>
+
+</article> -->
+
+<div class="sub_visual">
+    <div class="sub_txt">
+        <div class="cell">
+            <p id="sub_tit"></p>
+               <p class="sub_tit_s">제1회 공주디지털문화유산전<br> <span class="small">2021년 12월 18일(토) ~ 20일(월)
+</span></p>
+        </div>
+    </div>
+</div>
+ <script>
+       /* $(document).ready(function() {
+            if (oneDepth == 1) {
+                $(".sub_tit_s").html("");
+            } else {
+                $(".sub_tit_s").html("");
+            }
+
+        });*/
+
+    </script>
+
+                <!-- container -->
+                <div id="container">
+                    <div id="lnb_wrap">
+	<div class="lnb">
+	<p class="ico_home"><a href="../main/main.php.html">home</a></p>
+		<div class="lnbs lnbs1">
+			<h2 id="lnb_tit"></h2>
+			<ul class="copy_menu" id="gnb_menu">	
+		    </ul>
+		</div>
+		<div class="lnbs lnbs2">
+			<h3 id="lnb_tit2"></h3>
+			<ul class="copy_menu" id="lnb_menu">
+                <li class="lnb1 on"><a href="#;">행사소개</a></li>
+                <li class="lnb1"><a href="#;">부스배치도</a></li>
+                <li class="lnb1"><a href="#;">참가업체 리스트</a></li>
+			</ul>
+		</div>
+	</div>
+</div>                        <div id="inner_wrap">
+                            <div class="title_bar">
+    <h2 id="title">행사소개</h2>
+    
+</div>
+                                <!-- 내용 -->
+                                <div class="sub_con ">
+                                    <!-- <ul class="atab pt0">
+    <li><a href="01.php.html">대한민국<br class="br"> 교육박람회란?</a></li>
+    <li><a href="01_2.php.html">에듀테크<br class="br"> 코리아</a></li>
+    <li><a href="01_3.php.html">조기∙초등교육<br class="br"> 박람회</a></li>
+    <li><a href="01_4.php.html">평생∙자격증<br class="br">∙직업교육<br class="br"> 박람회</a></li>
+    <li><a href="http://educationkorea.kr/introduce/06.php" target="_blank">EDUCON</a></li>
+</ul> -->
+                                        <div class="introduce_box">
+                                            <p class="pic"><img src="../img/introduce/02_img01.jpg" class="" alt="">
+
+                                            </p>
+                                            <p class="first first01"><strong>GONGJU DIGITAL HERITAGE </strong><br>
+                                            제 1회 공주디지털문화박람회</p>
+                                            <span class="ico"><img src="../img/introduce/ico02.gif" alt=""></span>공주에서<br /> DIGITAL HERITAGE 박람회가 개최됩니다.</div>
+
+										<ul class="edu_ul">
+											<li><span><img src="../img/introduce/02_img02.gif" class="" alt=""></span></li>
+											<li><span><img src="../img/introduce/02_img03.gif" class="" alt=""></span></li>
+											<li><span><img src="../img/introduce/02_img04.jpg" class="" alt=""></span></li>
+											<li><span><img src="../img/introduce/02_img05.gif" class="" alt=""></span></li>
+										</ul>
+
+                                        <h3 class="s_tit">전시회 개요</h3>
+                                        <div class="sumW">
+											<ul class="summary_ul">
+												<li>
+													<strong class="txt_t">명 칭</strong> <span class="feb fcor0">제 11회 공주 디지털문화박람회<br />
+													GONGJU DIGITAL HERITAGE 2022<br />
+												</li>
+												<li>
+													<style>.gigan_hall {font-size:12px; display:inline-block; width:100px; text-align: center; line-height:22px; background:#dbc4ea; color:#000000; margin:0px 8px 0 0; border-radius:6px; vertical-align: middle}
+													</style>
+													<p><strong class="txt_t">기 간</strong> <!--<span class="gigan_hall"><strong>Business Hall</strong></span>1층 A, B홀 – 2021. 1. 14(목) ~ 16(토), 3일간-->2021. 12. 18. (토) ~ 12. 20. (월) 3일간 10:00 ~ 18:00</p>
+													<!--<p> <span class="gigan_hall"><strong>Public Hall</strong></span>3층 C홀 – 2021. 1. 14(목) ~ 17(일), 4일간</p>-->
+												</li>
+												<li>
+													<strong class="txt_t">장 소</strong> 공주 반죽동
+												</li>
+												<li>
+													<strong class="txt_t">규 모</strong> 20개사 1,40부스
+												</li>
+												<li>
+													<strong class="txt_t">주 제</strong> <span class="fb" style="color:#875394;">공주학 연구원</span>
+												</li>
+												<li>
+													<strong class="txt_t">주 최</strong> 공주시
+												</li>
+												<li>
+													<strong class="txt_t">후 원<br />(예정)</strong> 교육부, 과학기술정보통신부, 중소벤처기업부, 서울특별시교육청 외 16개 시·도교육청 등
+												</li>
+											</ul>
+											<p class="sum_pic"><img src="../img/introduce/02_img06.gif" class="" alt=""></p>
+
+											<h4 class="sss_tit mt50">글로벌 파트너</h4>
+											<ul class="g_partner">
+												<li><span><img src="../img/introduce/02_img07.gif" class="" alt=""></span></li>
+												<li><span><img src="../img/introduce/02_img08.gif" class="" alt=""></span></li>
+												<li><span><img src="../img/introduce/02_img09.gif" class="" alt=""></span></li>
+												<li><span><img src="../img/introduce/02_img10.gif" class="" alt=""></span></li>
+												<li><span><img src="../img/introduce/02_img11.gif" class="" alt=""></span></li>
+											</ul>
+										</div>
+                                        <!--<h3 class="s_tit">전시회 구성</h3>
+										<p><img src="../img/introduce/02_img12.gif"></p>-->
+
+                                </div>
+                                <!-- //내용 -->
+                        </div>
+                </div>
+                <!-- //container -->
