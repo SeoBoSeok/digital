f1277279e1d98af38da80f0c5ebc8cd8d91df3a9
SeoBoSeok <ggy7bbo@gmail.com>
2021-11-18 11:54:57 +0900

    ho tfix

--- theme/basic/skin/content/introduce/content.skin.php
+++ theme/basic/skin/content/introduce/content.skin.php
@@ -4,6 +4,7 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 // add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
 // add_stylesheet('<link rel="stylesheet" href="'.$content_skin_url.'/style.css">', 0);
 ?>
+<link rel="stylesheet" type="text/css" href="/common/css/layout.css">
 <link rel="stylesheet" type="text/css" href="/common/css/sub.css">
 <!-- <html lang="ko"><head> -->
     <!-- <meta charset="UTF-8">
