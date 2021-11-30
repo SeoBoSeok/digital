3a6d52277a31fecef3a53c3fe70169fd2554f51c
SeoBoSeok <ggy7bbo@gmail.com>
2021-11-30 15:09:12 +0900

    hot fix

--- theme/basic/header.part.php
+++ theme/basic/header.part.php
@@ -8,26 +8,26 @@
           <a href="/bbs/content.php?co_id=introduce">전시소개</a>
           <ul>
             <li><a href="/bbs/content.php?co_id=introduce">전시소개</a></li>
-            <li><a href="#">공간배치</a></li>
-            <li><a href="#">참가업체리스트</a></li>
+            <li><a href="/bbs/content.php?co_id=booth">공간배치</a></li>
+            <li><a href="/bbs/content.php?co_id=list">참가업체리스트</a></li>
           </ul>
         </li>
         <li>
-          <a href="#">참여기관</a>
+          <a href="/bbs/content.php?co_id=about_org">참여기관</a>
           <ul>
-            <li><a href="#">디지털유산 산업</a></li>
-            <li><a href="#">디지털유산 연구개발</a></li>
-            <li><a href="#">디지털유산 미래교육</a></li>
-            <li><a href="#">문화 유산상품</a></li>
+            <li><a href="/bbs/content.php?co_id=about_org">디지털유산 산업</a></li>
+            <li><a href="/bbs/content.php?co_id=about_org_dev">디지털유산 연구개발</a></li>
+            <li><a href="/bbs/content.php?co_id=about_org_edu">디지털유산 미래교육</a></li>
+            <li><a href="/bbs/content.php?co_id=about_org_product">문화 유산상품</a></li>
           </ul>
         </li>
         <li>
-          <a href="#">초청전</a>
+          <a href="/bbs/content.php?co_id=Invitation">초청전</a>
           <ul>
-            <li><a href="#">디지털로 만나는<br>백제역사유적지구</a></li>
-            <li><a href="#">소실된 웅진백제기공주<br>정지산유적고건축AR콘텐츠</a></li>
-            <li><a href="#">정조대왕의 수원화성br<br>야간군사 훈련 VAR 콘텐츠</a></li>
-            <li><a href="#">청동거울,<br>과학을 비추다</a></li>
+            <li><a href="/bbs/content.php?co_id=Invitation_baekjae">디지털로 만나는<br>백제역사유적지구</a></li>
+            <li><a href="/bbs/content.php?co_id=Invitation_baekjae_a">소실된 웅진백제기공주<br>정지산유적고건축AR콘텐츠</a></li>
+            <li><a href="/bbs/content.php?co_id=Invitation_baekjae_v">정조대왕의 수원화성<br>야간군사 훈련 VAR 콘텐츠</a></li>
+            <li><a href="/bbs/content.php?co_id=Invitation_baekjae_m">청동거울,<br>과학을 비추다</a></li>
           </ul>
         </li>
         <li>
