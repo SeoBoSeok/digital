2e32011f97925f37c908169d7a17317e466f6394
SeoBoSeok <ggy7bbo@gmail.com>
2021-12-10 09:59:34 +0900

    new pages

--- theme/basic/skin/content/tutorial/content.skin.php
+++ theme/basic/skin/content/tutorial/content.skin.php
@@ -113,7 +113,7 @@ echo '<link rel="stylesheet" href="'.$content_skin_url.'/style.css">';
               </dl>
               <dl>
                 <dt>사전신청</dt>
-                <dd><a href="/bbs/content.php?co_id=pre_forms">사전신청하기</a></dd>
+                <dd><a href="/bbs/content.php?co_id=pre_forms" target="_blank">사전신청하기</a></dd>
               </dl>
             </div>
           </div>
