5bb6c6e1d20d1190ccb01fdb09bcbc065335f11d
SeoBoSeok <ggy7bbo@gmail.com>
2021-12-15 13:03:58 +0900

    new pages

--- skin/member/basic/login.skin.php
+++ skin/member/basic/login.skin.php
@@ -2,110 +2,90 @@
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
 // add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
-echo '<link rel="stylesheet" href="'.$member_skin_url.'/style.css">';
-
+add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 0);
 ?>
+
 <!-- 로그인 시작 { -->
-<div id="mb_login" class="mbskin">
-    <div class="mbskin_box">
-        <h1><?php echo $g5['title'] ?></h1>
-        <div class="mb_log_cate">
-            <h2><span class="sound_only">회원</span>로그인</h2>
-            <a href="<?php echo G5_BBS_URL ?>/register.php" class="join">회원가입</a>
-        </div>
-        <form name="flogin" action="<?php echo $login_action_url ?>" onsubmit="return flogin_submit(this);" method="post">
-        <input type="hidden" name="url" value="<?php echo $login_url ?>">
-        
-        <fieldset id="login_fs">
-            <legend>회원로그인</legend>
-            <label for="login_id" class="sound_only">회원아이디<strong class="sound_only"> 필수</strong></label>
-            <input type="text" name="mb_id" id="login_id" required class="frm_input required" size="20" maxLength="20" placeholder="아이디">
-            <label for="login_pw" class="sound_only">비밀번호<strong class="sound_only"> 필수</strong></label>
-            <input type="password" name="mb_password" id="login_pw" required class="frm_input required" size="20" maxLength="20" placeholder="비밀번호">
-            <button type="submit" class="btn_submit">로그인</button>
-            
-            <div id="login_info">
-                <div class="login_if_auto chk_box">
-                    <input type="checkbox" name="auto_login" id="login_auto_login" class="selec_chk">
-                    <label for="login_auto_login"><span></span> 자동로그인</label>  
+    <?php
+if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+
+// add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
+// add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 0);
+echo '<link rel="stylesheet" href="'.$member_skin_url.'/style.css">';
+?>
+
+<div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image" style="background-image: url('/common/img/newposter.jpg'); background-position: top"></div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">공주 디지털 문화 유산전 관리자페이지</h1>
+                                    </div>
+                                    <form name="flogin" action="<?php echo $login_action_url ?>" onsubmit="return flogin_submit(this);" method="post">
+                                        <input type="hidden" name="url" value="<?php echo $login_url ?>">
+                                        <div class="form-group">
+                                            <input type="text" name="mb_id" class="form-control form-control-user"
+                                                id="login_id" aria-describedby="emailHelp"
+                                                placeholder="Enter ID...">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" name="mb_password" class="form-control form-control-user"
+                                                id="login_pw" placeholder="Password">
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="custom-control custom-checkbox small">
+                                                <input type="checkbox" class="custom-control-input" id="customCheck">
+                                                <label class="custom-control-label" for="customCheck">Remember
+                                                    Me</label>
+                                            </div>
+                                        </div>
+                                        <!-- <a href="#" type="submit" class="btn btn-primary btn-user btn-block">
+                                            Login
+                                        </a> -->
+                                        <button type="submit" class="btn_submit" style="color: #fff;
+    background-color: #081641;
+    border-color: #081641; width: 100%;padding: 10px; border-radius: 10px;">로그인</button>
+                                        <!-- <hr>
+                                        <a href="index.html" class="btn btn-google btn-user btn-block">
+                                            <i class="fab fa-google fa-fw"></i> Login with Google
+                                        </a>
+                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
+                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                                        </a> -->
+
+                                    </form>
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                    </div>
+                                    <div class="text-center">
+                                        <a class="small" href="register.html">Create an Account!</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="login_if_lpl">
-                    <a href="<?php echo G5_BBS_URL ?>/password_lost.php" target="_blank" id="login_password_lost">정보찾기</a>  
-                </div>
+
             </div>
-        </fieldset> 
-        </form>
-        <?php @include_once(get_social_skin_path().'/social_login.skin.php'); // 소셜로그인 사용시 소셜로그인 버튼 ?>
-    </div>
 
-    <?php // 쇼핑몰 사용시 여기부터 ?>
-    <?php if (isset($default['de_level_sell']) && $default['de_level_sell'] == 1) { // 상품구입 권한 ?>
-
-	<!-- 주문하기, 신청하기 -->
-	<?php if (preg_match("/orderform.php/", $url)) { ?>
-    <section id="mb_login_notmb">
-        <h2>비회원 구매</h2>
-        <p>비회원으로 주문하시는 경우 포인트는 지급하지 않습니다.</p>
-
-        <div id="guest_privacy">
-            <?php echo conv_content($default['de_guest_privacy'], $config['cf_editor']); ?>
-        </div>
-		
-		<div class="chk_box">
-			<input type="checkbox" id="agree" value="1" class="selec_chk">
-        	<label for="agree"><span></span> 개인정보수집에 대한 내용을 읽었으며 이에 동의합니다.</label>
-		</div>
-		
-        <div class="btn_confirm">
-            <a href="javascript:guest_submit(document.flogin);" class="btn_submit">비회원으로 구매하기</a>
         </div>
 
-        <script>
-        function guest_submit(f)
-        {
-            if (document.getElementById('agree')) {
-                if (!document.getElementById('agree').checked) {
-                    alert("개인정보수집에 대한 내용을 읽고 이에 동의하셔야 합니다.");
-                    return;
-                }
-            }
-
-            f.url.value = "<?php echo $url; ?>";
-            f.action = "<?php echo $url; ?>";
-            f.submit();
-        }
-        </script>
-    </section>
-
-    <?php } else if (preg_match("/orderinquiry.php$/", $url)) { ?>
-    <div id="mb_login_od_wr">
-        <h2>비회원 주문조회 </h2>
-
-        <fieldset id="mb_login_od">
-            <legend>비회원 주문조회</legend>
-
-            <form name="forderinquiry" method="post" action="<?php echo urldecode($url); ?>" autocomplete="off">
-
-            <label for="od_id" class="od_id sound_only">주문서번호<strong class="sound_only"> 필수</strong></label>
-            <input type="text" name="od_id" value="<?php echo $od_id; ?>" id="od_id" required class="frm_input required" size="20" placeholder="주문서번호">
-            <label for="od_pwd" class="od_pwd sound_only">비밀번호 <strong>필수</strong></label>
-            <input type="password" name="od_pwd" size="20" id="od_pwd" required class="frm_input required" placeholder="비밀번호">
-            <button type="submit" class="btn_submit">확인</button>
-
-            </form>
-        </fieldset>
-
-        <section id="mb_login_odinfo">
-            <p>메일로 발송해드린 주문서의 <strong>주문번호</strong> 및 주문 시 입력하신 <strong>비밀번호</strong>를 정확히 입력해주십시오.</p>
-        </section>
-
     </div>
-    <?php } ?>
 
-    <?php } ?>
-    <?php // 쇼핑몰 사용시 여기까지 반드시 복사해 넣으세요 ?>
 
-</div>
+
+
 
 <script>
 jQuery(function($){
@@ -125,4 +105,3 @@ function flogin_submit(f)
 }
 </script>
 <!-- } 로그인 끝 -->
-
