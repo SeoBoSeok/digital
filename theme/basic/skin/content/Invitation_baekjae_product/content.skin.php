573344c9710df66555b6b96a7a38dfc9e0a26d5e
SeoBoSeok <ggy7bbo@gmail.com>
2021-12-15 13:58:55 +0900

    new pages

--- theme/basic/skin/content/Invitation_baekjae_product/content.skin.php
+++ theme/basic/skin/content/Invitation_baekjae_product/content.skin.php
@@ -21,7 +21,7 @@ echo '<link rel="stylesheet" href="'.$content_skin_url.'/style.css">';
           </div>
           <div class="wrap">
             <h2>초청전</h2>
-            <p>문화유산, 디지털과 만나다.</p>
+            <p>문화유산, 디지털과 만나다</p>
           </div>
         </div>
       </section>
