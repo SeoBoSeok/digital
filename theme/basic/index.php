7c6341c4aac594b906840eba50592df8e5ff0ff3
SeoBoSeok <ggy7bbo@gmail.com>
2021-12-17 01:38:04 +0900

    hot fix

--- theme/basic/index.php
+++ theme/basic/index.php
@@ -113,8 +113,8 @@ if(G5_COMMUNITY_USE === false) {
     <div id="hd_pop">
       <h2>팝업레이어</h2>
       <div id="hd_pops_1" class="hd_pops" style="top:100px;left:10px">
-      <a href="/bbs/content.php?co_id=tutorial_forms" style="display: block;"><div class="hd_pops_con" style="width:400px;height:400px">
-              <p><img src="https://gongju-digitage.co.kr/common/img/popup1.jpg" title="fb4b9488897987bc4163859f8c1ae739_1639462739_327.png" alt="fb4b9488897987bc4163859f8c1ae739_1639462739_327.png"><br style="clear:both;">&nbsp;</p>        </div></a>
+      <a href="/bbs/content.php?co_id=tutorial_forms" style="display: block;"><div class="hd_pops_con" style="width:450px;height:565px">
+              <p><img src="https://gongju-digitage.co.kr/common/img/popup_tutorial.jpg" title="fb4b9488897987bc4163859f8c1ae739_1639462739_327.png" alt="fb4b9488897987bc4163859f8c1ae739_1639462739_327.png"><br style="clear:both;">&nbsp;</p>        </div></a>
           <div class="hd_pops_footer">
               <button class="hd_pops_reject hd_pops_1 24"><strong>24</strong>시간 동안 다시 열람하지 않습니다.</button>
               <button class="hd_pops_close hd_pops_1">닫기 <i class="fa fa-times" aria-hidden="true"></i></button>
