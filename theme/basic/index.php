138308ec5af2f0b45c6c6036219d07ae0d01478d
SeoBoSeok <ggy7bbo@gmail.com>
2022-05-17 07:06:53 +0900

    hot fix

--- theme/basic/index.php
+++ theme/basic/index.php
@@ -43,10 +43,10 @@ if(G5_COMMUNITY_USE === false) {
     <link rel="apple-touch-icon" sizes="144x144" href="common/img/apple-icon-144x144.png">
     <link rel="apple-touch-icon" sizes="152x152" href="common/img/apple-icon-152x152.png">
     <link rel="apple-touch-icon" sizes="180x180" href="common/img/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="common/img/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="common/img/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="common/img/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="common/img/favicon-16x16.png">
+    <!-- <link rel="icon" type="image/png" sizes="192x192"  href="common/img/android-icon-192x192.png"> -->
+    <!-- <link rel="icon" type="image/png" sizes="32x32" href="common/img/favicon-32x32.png"> -->
+    <!-- <link rel="icon" type="image/png" sizes="96x96" href="common/img/favicon-96x96.png"> -->
+    <!-- <link rel="icon" type="image/png" sizes="16x16" href="common/img/favicon-16x16.png"> -->
     <!-- <link rel="manifest" href="common/img/manifest.json"> -->
     <!-- <meta name="msapplication-TileColor" content="#ffffff"> -->
     <!-- <meta name="msapplication-TileImage" content="common/img/ms-icon-144x144.png"> -->
